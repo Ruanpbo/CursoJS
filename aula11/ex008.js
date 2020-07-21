5033c41a9943034dd68a8251ae88ce5c5c9832c7
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-21 18:37:45 -0300

    Comandos DOM
    ex.08, 09, 10 teste
    
    ex11, exercicio pratico

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro e de ${vel}km/h`)
+if (vel > 60){//condicao simples!
+    console.log(`voce ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`)
