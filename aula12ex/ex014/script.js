e7faf2db83bb67bae122f8e754eccbf2188a3897
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-23 17:56:00 -0300

    Trabalho com img

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,17 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora sao ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // Bom dia!
+        msg.innerHTML = `Bom dia! Agora sao ${hora} horas.`
         img.src = 'fotomanha.png'
         document.body.style.background = '#baccda'
     } else if ( hora >= 12 && hora < 18) {
         //Boa tarde!
+        msg.innerHTML = `Boa tarde! Agora sao ${hora} horas.`
         img.src = 'fototarde.png'
         document.body.style.background = '#f4b25b'
     } else {
         //Boa noite!
+        msg.innerHTML = `Boa noite! Agora sao ${hora} horas.`
         img.src = 'fotonoite.png'
         document.body.style.background = '#4d4d4d'
     }
