9e5f5d189960864a43d39c82c1f74995699a9693
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-24 18:53:26 -0300

    aula

---
+++ aula14ex/ex16/script.js
@@ -0,0 +1,6 @@
+function contar(){
+    let ini = document.querySelector('div#txti')
+    let fim = document.querySelector('div#txtf')
+    let passo = document.querySelector('div#txtp')
+    let res = document.querySelector('div#resultado')    
+}
