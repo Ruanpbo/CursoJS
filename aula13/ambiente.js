38b5fb2573ce39ac1a1a93ba7ce222919c082a71
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-24 12:40:13 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,1 +1,44 @@
-console.log('Tudo bem?')
+var c = 1
+    do {
+        console.log(`${c}° Passo`)
+        c++
+    } while(c <= 6) 
+
+
+/*var c = 1
+    while(c <= 500){
+        console.log(`${c}° Passo`)
+        c++
+    }
+
+console.log('Tudo bem?')*/
+
+/*var pizza = 'pequena'
+var c = 1
+if ( pizza == 'broto') {
+    while (c <= 4){
+        console.log(`Comer ${c}° fatia`)
+        c++
+    }
+} else if ( pizza == 'pequena') {
+    while (c <= 6){ 
+        console.log(`Comer ${c}° fatia `)
+        c++
+    }
+}else if ( pizza == 'media') {
+    while (c <= 9){
+        console.log(`Comer ${c}° fatia`)
+        c++
+    }
+}else if (pizza == 'grande') {
+    while (c <= 12){
+        console.log(`Comer ${c}° fatia`)
+        c++
+    }
+} else if (pizza == 'gigante') {
+    while (c <= 18) {
+        console.log(`Comer ${c}° fatia`)
+        c++
+    }
+}
+*/
