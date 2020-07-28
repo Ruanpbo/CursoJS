71593c3ba2cdab2bc7262c352647e28834ef0de9
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-28 18:02:55 -0300

    Analisador de Numeros

---
+++ aula16ex/modelo/script.js
@@ -0,0 +1,11 @@
+function adicionar (){
+    let num = window.document.getElementById('txtnum')
+    let res = window.document.getElementById('resultado')
+    n = Number(num.value)
+    if ( n < 1 || n > 100 ) {
+        window.alert('Valor invalido ou já encontrado na lista!')
+    } else {
+        
+    }
+     
+}
