5f5b07de6d4cd578a153256649b1fc11d87c95d3
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-28 18:40:07 -0300

    RESOLUCAO

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -1,11 +1,28 @@
-function adicionar (){
-    let num = window.document.getElementById('txtnum')
-    let res = window.document.getElementById('resultado')
-    n = Number(num.value)
-    if ( n < 1 || n > 100 ) {
-        window.alert('Valor invalido ou já encontrado na lista!')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <=100) {
+        return true
     } else {
-        
+        return false
     }
-     
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores) ) {
+        window.alert('Tudo ok')
+    }  else {
+        window.alert('Valor invalido ou ja encontrado na lista.')
+    }
 }
