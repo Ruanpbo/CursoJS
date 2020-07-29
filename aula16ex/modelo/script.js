ead23b267d41a1be5ed44794d44a71667cfad302
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-29 17:07:42 -0300

    primeira parte

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -1,7 +1,7 @@
 let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
-let valores = []
+let valores = [] //Criando o vetor vazio
 
 function isNumero(n) {
     if (Number(n) >= 1 && Number(n) <=100) {
@@ -21,8 +21,13 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores) ) {
-        window.alert('Tudo ok')
+        valores.push(Number(num.value)) //adicionando valor no vetor e na lista
+        let item = document.createElement('option') //Criando uma variavel dinamica
+        item.text = `Valor ${num.value} adicionado` // texto que deve aparecer na lista 
+        lista.appendChild(item) // modo de fazer o texto aparecer na lista
     }  else {
         window.alert('Valor invalido ou ja encontrado na lista.')
     }
+    num.value = '' //limpar espaco da caixa de texto que coloca o  numero
+    num.focus() //fazer o cursor piscar na caixa de texto
 }
