0ac60463ec5beee5b48b665cba1beae6a9e4acfe
Ruanpbo <Ruanpbo@hotmail.com>
2020-07-29 17:42:20 -0300

    resolução

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -25,9 +25,36 @@ function adicionar() {
         let item = document.createElement('option') //Criando uma variavel dinamica
         item.text = `Valor ${num.value} adicionado` // texto que deve aparecer na lista 
         lista.appendChild(item) // modo de fazer o texto aparecer na lista
+        res.innerHTML = '' // Caso adicione mais dados, apaga os resultados 
     }  else {
         window.alert('Valor invalido ou ja encontrado na lista.')
     }
     num.value = '' //limpar espaco da caixa de texto que coloca o  numero
     num.focus() //fazer o cursor piscar na caixa de texto
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos] 
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<P>O menor valor informado foi ${menor}</P>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A media dos valores digitado e ${media}.</p>`
+    }
 }
